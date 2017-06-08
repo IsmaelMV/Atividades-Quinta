d8b6ec9df5daa6d6dbd3704475fdcfbcc5eaf9bb
IsmaelMV <maelsurf2005@gmail.com>
2017-06-08 20:45:27 -0300

    Add files via upload

---
+++ Pomodoro/Pomodoro/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Pomodoro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTempo = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblestado = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.lblTempo2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tempo de Atividade:";
+            // 
+            // lblTempo
+            // 
+            this.lblTempo.AutoSize = true;
+            this.lblTempo.Location = new System.Drawing.Point(143, 39);
+            this.lblTempo.Name = "lblTempo";
+            this.lblTempo.Size = new System.Drawing.Size(49, 13);
+            this.lblTempo.TabIndex = 1;
+            this.lblTempo.Text = "00:00:00";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 91);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(108, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Tempo de descanço:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(239, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 22);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Ativar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(239, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 21);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Ativar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblestado
+            // 
+            this.lblestado.AutoSize = true;
+            this.lblestado.Location = new System.Drawing.Point(143, 151);
+            this.lblestado.Name = "lblestado";
+            this.lblestado.Size = new System.Drawing.Size(25, 13);
+            this.lblestado.TabIndex = 14;
+            this.lblestado.Text = "(....)";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 750;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 750;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // lblTempo2
+            // 
+            this.lblTempo2.AutoSize = true;
+            this.lblTempo2.Location = new System.Drawing.Point(143, 91);
+            this.lblTempo2.Name = "lblTempo2";
+            this.lblTempo2.Size = new System.Drawing.Size(49, 13);
+            this.lblTempo2.TabIndex = 15;
+            this.lblTempo2.Text = "00:00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 195);
+            this.Controls.Add(this.lblTempo2);
+            this.Controls.Add(this.lblestado);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblTempo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Pomodoro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTempo;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblestado;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label lblTempo2;
+    }
+}
+
---
+++ Pomodoro/Pomodoro/Pomodoro/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pomodoro
+{
+    public partial class Form1 : Form
+    {
+        int  hora = 0, min = 0, seg = 0,hora2 = 0, min2 = 0, seg2 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            lblestado.Text = "Praticar atividade";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = true;
+            lblestado.Text = "Descanse";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            seg++;
+            if (seg == 60)
+            {
+                min++;
+                seg = 0;
+            }
+
+            else if (min == 60)
+            {
+                hora++;
+                min = 0;
+            }
+
+            lblTempo.Text = hora.ToString().PadLeft(2, '0') + ":" + min.ToString().PadLeft(2, '0') + ":" + seg.ToString().PadLeft(2, '0');
+
+            if (lblTempo.Text == "00:00:25")
+                {
+                    timer1.Enabled = false;
+                    MessageBox.Show("Tempo de Atividade Encerrado, ative o seu tempo de descanso");
+                    button2.Enabled = true;
+                    lblTempo.Text = "00:00:00";
+                    button1.Enabled = false; 
+                    
+
+                }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            seg2++;
+            if (seg == 60)
+            {
+                min2++;
+                seg = 0;
+            }
+
+            else if (min == 60)
+            {
+                hora2++;
+                min = 0;
+            }
+
+            lblTempo2.Text = hora2.ToString().PadLeft(2, '0') + ":" + min2.ToString().PadLeft(2, '0') + ":" + seg2.ToString().PadLeft(2, '0');
+
+            if (lblTempo2.Text == "00:00:10")
+            {
+                timer2.Enabled = false;
+                MessageBox.Show("Tempo de descanso Encerrado, ative o seu tempo de atividade");
+                //button2.Enabled = true;
+                lblTempo2.Text = "00:00:00";
+                button2.Enabled = false;
+                button1.Enabled = true;
+                seg = 0;
+                seg2 = 0;
+                min = 0;
+                min2 = 0;
+                hora = 0;
+                hora2 = 0;
+
+            }
+        }
+    }
+}
